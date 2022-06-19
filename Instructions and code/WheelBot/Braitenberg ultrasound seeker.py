dd6a35e91f1c98aebece14bf44d0cfe2f87fcf2a
shepai <dextershepherd@hotmail.co.uk>
2022-06-19 18:39:37 +0100

    seeker

--- Instructions and code/WheelBot/Braitenberg ultrasound seeker.py
+++ Instructions and code/WheelBot/Braitenberg ultrasound seeker.py
@@ -1,3 +1,15 @@
+"""
+Braitenbergs are simple agents that exhibit seeking behaviour.
+This code uses the ultrasound sensor to run towards from obstacles.
+
+If the robot is not moving towards the target, consider changing the ">" and "<"
+symbols in lines 31 and 28 to be the opposite way round to go left and right. 
+
+Code by Dexter R Shepherd
+
+"""
+
+
 from EduBot import WheelBot
 import utime
 
@@ -7,20 +19,21 @@ robot.stop()
 
 
 while True:
-    
+
     dist=robot.distance()
+
     #print(dist)
-    if abs(dist[0]-dist[1])<10 or (dist[0]>40 and dist[1]>40): #insignificant distance
-        print("f")
-        robot.forward()
+    if dist[0]<10 and dist[1]<10: #insignificant distance
+        robot.stop()
     elif dist[0]>dist[1]: #ione sensor clsoer than othre
         print("r")
         robot.right(delay=0.5)
- 
-    else: #other sensor closer than sensor
+    elif dist[0]<dist[1]: #other sensor closer than sensor
         print("l")
         robot.left(delay=0.5)
+    else:
+        robot.forward()
 
-    
+
     utime.sleep(1)
 robot.stop()
