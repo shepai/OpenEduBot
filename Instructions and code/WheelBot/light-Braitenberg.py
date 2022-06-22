2b79f66a6e347e029391ddb668b62889b11ddfc6
shepai <dextershepherd@hotmail.co.uk>
2022-06-22 09:17:47 +0100

    documentation

--- Instructions and code/WheelBot/light-Braitenberg.py
+++ Instructions and code/WheelBot/light-Braitenberg.py
@@ -1,6 +1,8 @@
 """
 Light braightenberg robot
-The light sensors allow close quarter sensing incase of collision.
+The light sensors allow light following behaviours.
+This robot design does not use motor speed like the original design suggests for
+simplicity of robot.
 
 The sensor pins are read at 26 and 27, if the robot is not turning away from the collision,
 consider switching the pins around (26 & 27).
@@ -14,12 +16,10 @@ import utime
 from machine import Pin,ADC
 from random import choice
 
-robot = WheelBot(trigPin=[12,5],echoPin=[15,4],in1=11,in2=10,in3=9,in4=8)
+robot = WheelBot(trigPin=[12,5],echoPin=[15,4],in1=11,in2=10,in3=9,in4=8) #wheel bot chassis pins
 robot.stop()
 
-sensor1=Pin(26,Pin.IN)
-sensor2=Pin(27,Pin.IN)
-
+#take pins as analog as to get a varied reading
 sensor1=ADC(26)
 sensor2=ADC(27)
 
@@ -32,22 +32,18 @@ while True:
     print(s1,s2)
     if (dist[0]<=10 and dist[1]<=10): #insignificant distance
         robot.stop()
-    elif abs(s2-s1)<150: #noise 
+    elif abs(s2-s1)<150: #filter noise and go forward if no light is found
+        #more elegant solutions would rotate around and find the lightest angle
         print("f")
         robot.forward()
         utime.sleep(0.8)
-        robot.right(delay=choice([0.15,0.25])) #use random choice so that the robot doesn't get stuck turning left and right
-        robot.stop()
-    elif s1>s2: #just one obstacle
+        robot.stop() #implement stops
+    elif s1>s2: #move towards the lightest sensor
         print("r")
-        #robot.backward()
-        #utime.sleep(0.8)
         robot.right(delay=0.05)
         robot.stop()
-    elif s2>s1: #just one obstacle
+    elif s2>s1: #move towards the lightest sensor
         print("l")
-        #robot.backward()
-        #utime.sleep(0.8)
         robot.left(delay=0.05)
         robot.stop()
     
