698519e935d5bc4e80b427cfe8f01c029cc74339
shepai <dextershepherd@hotmail.co.uk>
2022-07-22 14:53:33 +0100

    forward

--- Instructions and code/ServoBot/main.py
+++ Instructions and code/ServoBot/main.py
@@ -10,12 +10,15 @@ import utime
 
 
 a=servoBot(1,2)
-val=10
-a.servoWrite(1,0)
-a.servoWrite(2,0)
-utime.sleep(1)
-for i in range(10): 
-    a.servoWrite(1,val+10)
-    a.servoWrite(2,val+10)
+
+a.servoWrite(1,40)
+a.servoWrite(2,140)
+"""
+for i in range(10):
+    a.servoWrite(1,0)
+    a.servoWrite(2,0)
     utime.sleep(1)
-    
+    a.servoWrite(1,180)
+    a.servoWrite(2,180)
+    utime.sleep(1)
+    """
