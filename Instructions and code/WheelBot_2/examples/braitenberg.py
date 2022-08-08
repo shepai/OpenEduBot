58f31c9a1516f11a48f45d74f26d6ab09fdfd6e8
shepai <dextershepherd@hotmail.co.uk>
2022-08-08 09:54:09 +0100

    new instructions

--- Instructions and code/WheelBot_2/examples/braitenberg.py
+++ Instructions and code/WheelBot_2/examples/braitenberg.py
@@ -1,6 +1,6 @@
 """
 Light braightenberg robot
-The light sensors allow light following behaviours.
+The light sensors allow close quarter sensing incase of collision.
 
 The sensor pins are read at 26 and 27, if the robot is not turning away from the collision,
 consider switching the pins around (26 & 27).
@@ -9,37 +9,40 @@ Code by Dexter R Shepherd
 
 """
 
-from EduBot import WheelBot
+from EduBot import wheelBot_2
 import utime
 from machine import Pin,ADC
 from random import choice
 
-robot = WheelBot() #wheel bot chassis pins
+#setup the robot
+robot = wheelBot_2()
 robot.stop()
 
-#take pins as analog as to get a varied reading
+sensor1=Pin(26,Pin.IN)
+sensor2=Pin(27,Pin.IN)
+
 sensor1=ADC(26)
 sensor2=ADC(27)
 
 while True:
+    #gather sensor data
     s1=int(sensor1.read_u16()/10)
     s2=int(sensor2.read_u16()/10)
-    print(s1,s2)
-    if abs(s2-s1)<150: #filter noise and go forward if no light is found
-        #more elegant solutions would rotate around and find the lightest angle
-        print("f")
-        robot.forward()
-        utime.sleep(0.8)
-        robot.stop() #implement stops
-    elif s1>s2: #move towards the lightest sensor
-        print("r")
-        robot.right(delay=0.05)
-        robot.stop()
-    elif s2>s1: #move towards the lightest sensor
-        print("l")
-        robot.left(delay=0.05)
-        robot.stop()
 
+    print(s1,s2)
+    
+    if abs(s2-s1)<150: #noise 
+        robot.forward(20)
+        utime.sleep(0.8)
+        robot.stop()
+    elif s1>s2: #just one obstacle
+        robot.left(delay=0.25)
+        robot.stop()
+    elif s2>s1: #just one obstacle
+        robot.right(delay=0.25)
+        robot.stop()
 
     utime.sleep(0.5)
 robot.stop()
+
+
