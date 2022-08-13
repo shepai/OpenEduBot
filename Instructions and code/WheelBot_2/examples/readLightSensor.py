c25b3aa425de5d634bcaff9ae8a31cb4d2900398
shepai <dextershepherd@hotmail.co.uk>
2022-08-13 14:19:49 +0100

    no negative

--- Instructions and code/WheelBot_2/examples/readLightSensor.py
+++ Instructions and code/WheelBot_2/examples/readLightSensor.py
@@ -3,7 +3,7 @@ import utime
 
 sensor1=ADC(26)
 def getReading(sen):
-    return (200-sen.read_u16()//100)/200
+    return max((200-sen.read_u16()//100)/200,0)
 while True:
     utime.sleep(0.5)
-    print(getReading(sensor1))
+    print(getReading(sensor1)*100,"% Light intensity")
