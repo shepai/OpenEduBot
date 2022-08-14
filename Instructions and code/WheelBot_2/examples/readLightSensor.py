608546279d6dcad2804f19c0e021673f3e3bdc0f
shepai <dextershepherd@hotmail.co.uk>
2022-08-14 13:12:23 +0100

    led display

--- Instructions and code/WheelBot_2/examples/readLightSensor.py
+++ Instructions and code/WheelBot_2/examples/readLightSensor.py
@@ -2,8 +2,28 @@ from machine import Pin,ADC
 import utime
 
 sensor1=ADC(26)
+led=[Pin(2,Pin.OUT),Pin(3,Pin.OUT),Pin(4,Pin.OUT)]
+
 def getReading(sen):
     return max((200-sen.read_u16()//100)/200,0)
+print(dir(led[0]))
 while True:
     utime.sleep(0.5)
-    print(getReading(sensor1)*100,"% Light intensity")
+    val=getReading(sensor1)*100
+    print(val,"% Light intensity")
+    if val>80:
+        led[2].high()
+        led[1].high()
+        led[0].high()
+    elif val>65:
+        led[2].low()
+        led[1].high()
+        led[0].high()
+    elif val>50:
+        led[2].low()
+        led[1].low()
+        led[0].high()
+    else:
+        led[2].low()
+        led[1].low()
+        led[0].low()
