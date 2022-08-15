10e034528a9e8e48de2da5b08d816c8fe2a72e65
shepai <dextershepherd@hotmail.co.uk>
2022-08-15 10:20:27 +0100

    sensor example

---
+++ Instructions and code/CircuitPython WheelBot/sensor example.py
@@ -0,0 +1,44 @@
+import board
+import digitalio
+from analogio import AnalogIn
+import time
+import pwmio
+
+
+LED_PINS = [board.GP0, 
+            board.GP1,
+            board.GP2,
+            board.GP3,
+            board.GP4,
+            board.GP5,
+            board.GP6,
+            board.GP7,
+            board.GP16,
+            board.GP17]
+
+LEDS = []
+for pin in LED_PINS:
+    # Set pins as digital output
+    digout = digitalio.DigitalInOut(pin)
+    digout.direction = digitalio.Direction.OUTPUT
+    LEDS.append(digout)
+    
+sensor = AnalogIn(board.A0)
+
+def showPins(val):
+    for pin in LEDS:
+        pin.value=False
+    ind=int(len(LEDS)*val)
+    for i in range(ind):
+        LEDS[i].value=True
+def get_voltage(pin):
+    return max(min(1-((pin.value * 3.3) / 65536),1),0)
+
+
+while True:
+    intensity=get_voltage(sensor)
+    print((intensity,"% light intensity"))
+    showPins(intensity)
+    time.sleep(0.1)
+    
+
