fcad6dd78f441cdef967abeaa12a6936e09c1875
shepai <dextershepherd@hotmail.co.uk>
2022-08-19 15:40:08 +0100

    genotype bot read from new sensor

--- Instructions and code/WheelBot_2/examples/braitenberg_genotype.py
+++ Instructions and code/WheelBot_2/examples/braitenberg_genotype.py
@@ -13,10 +13,21 @@ from EduBot import wheelBot_2
 import utime
 from machine import ADC
 
-GENOTYPE=[ 8.08404537,  9.64568124,  2.99583673,  9.69069223,  0.60330581, -0.10944596]
-a=wheelBot_2()
+GENOTYPE=[ 8.08404537,  9.64568124,  2.99583673,  9.69069223,  0.60330581, -0.10944596] #add genotype
+
+robot=wheelBot_2() #the robot from the library
+
+#setup the pins for analog read
 sensor1=ADC(26)
 sensor2=ADC(27)
+
+#read background noise
+ST1=sensor1.read_u16()//100
+ST2=sensor1.read_u16()//100
+
+def getReading(sen,ST=200):
+    #read the sensor pin and convert to percentage of light
+    return max((ST-sen.read_u16()//100)/ST,0.01)
 
 #change the sensor gain to preference
 sensor_gain=0.5
@@ -24,8 +35,8 @@ sensor_gain=0.5
 def calc(genotype):
     w_ll,w_lr,w_rl,w_rr,bl,br = genotype
     # Calculate (square) distance to element
-    dl=int(sensor1.read_u16()/1000)
-    dr=int(sensor2.read_u16()/1000)
+    dl=getReading(sensor1,ST=ST1)
+    dr=getReading(sensor2,ST=ST2)
 
     #  Calculate local intensity
     il = sensor_gain/dl;
@@ -41,8 +52,17 @@ def calc(genotype):
 
 while True:
     speed1,speed2=calc(GENOTYPE) #place your genotype in here
-    utime.sleep(1)
     #do not change this code
     #pin directions speed
-    a.motorOn(4,"f",100*speed1)
-    a.motorOn(3,"f",100*speed2)
+    
+    #still keep slow or moving forward
+    speed_1=max(speed1//2,20)
+    speed_2=max(speed2//2,20)
+    
+    robot.motorOn(4, "f", speed_1)
+    robot.motorOn(3, "f", speed_2)
+    
+    #delay movement
+    utime.sleep(0.5)
+    robot.stop()
+    utime.sleep(0.2)
