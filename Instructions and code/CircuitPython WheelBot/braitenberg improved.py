fdcc6ff767cb8ce178fdfd0237216b01dded9f3f
shepai <dextershepherd@hotmail.co.uk>
2022-08-19 15:38:12 +0100

    new code for control

---
+++ Instructions and code/CircuitPython WheelBot/braitenberg improved.py
@@ -0,0 +1,29 @@
+"""
+
+"""
+
+from EduBot_CP import wheelBot_2
+import time
+import board
+from analogio import AnalogIn
+
+s1 = AnalogIn(board.GP26)
+s2 = AnalogIn(board.GP27)
+
+bot=wheelBot_2()
+
+def get_intensity(pin):
+    return max(min(1-((pin.value * 3.3) / 65536),1),0)
+
+while True:
+    r1,r2=(get_intensity(s1)*100,get_intensity(s2)*100)
+    
+    print(r1,r2)
+    bot.motor1_move(r1/2)
+    bot.motor2_move(r2/2)
+
+    time.sleep(0.5)
+    bot.stop()
+
+
+
---
+++ Instructions and code/WheelBot_2/examples/braitenberg improved.py
@@ -0,0 +1,47 @@
+"""
+Light braightenberg robot
+
+The sensor pins are read at 26 and 27, if the robot is not turning away from the collision,
+consider switching the pins around (26 & 27).
+
+Code by Dexter R Shepherd
+
+"""
+
+from EduBot import wheelBot_2
+import utime
+from machine import Pin,ADC
+from random import choice
+
+#setup the robot
+robot = wheelBot_2()
+robot.stop()
+
+sensor1=ADC(26)
+sensor2=ADC(27)
+
+#read background noise
+ST1=sensor1.read_u16()//100
+ST2=sensor1.read_u16()//100
+
+def getReading(sen,ST=200):
+    return max((ST-sen.read_u16()//100)/ST,0)
+
+while True:
+    #gather sensor data
+    s1=getReading(sensor1,ST=ST1)
+    s2=getReading(sensor2,ST=ST2)
+    
+    print(s1,s2)
+    #still keep slow or moving forward
+    speed_1=max(100*s1//2,20)
+    speed_2=max(100*s2//2,20)
+    
+    robot.motorOn(4, "f", speed_1)
+    robot.motorOn(3, "f", speed_2)
+    
+    utime.sleep(0.5)
+    robot.stop()
+    utime.sleep(0.1)
+
+
