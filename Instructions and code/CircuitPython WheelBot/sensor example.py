b212b2e7be2a36e37d01194ac892fbe759463f39
shepai <dextershepherd@hotmail.co.uk>
2022-09-02 12:33:48 +0100

    raw pin sensor code

--- Instructions and code/CircuitPython WheelBot/sensor example.py
+++ Instructions and code/CircuitPython WheelBot/sensor example.py
@@ -5,7 +5,7 @@ import time
 import pwmio
 
 
-LED_PINS = [board.GP0, 
+LED_PINS = [board.GP0,
             board.GP1,
             board.GP2,
             board.GP3,
@@ -22,7 +22,7 @@ for pin in LED_PINS:
     digout = digitalio.DigitalInOut(pin)
     digout.direction = digitalio.Direction.OUTPUT
     LEDS.append(digout)
-    
+
 sensor = AnalogIn(board.A0)
 
 def showPins(val):
@@ -31,15 +31,16 @@ def showPins(val):
     ind=int(len(LEDS)*val)
     for i in range(ind):
         LEDS[i].value=True
-        
+
 def get_voltage(pin):
     return max(min(1-((pin.value * 3.3) / 65536),1),0)
 
+def getRawPin(pin):
+    #this is if you have made your own sensor
+    return (sensor.value / 65535 * pin.reference_voltage)/3.7
 
 while True:
     intensity=get_voltage(sensor)
     print((intensity,"% light intensity"))
     showPins(intensity)
     time.sleep(0.1)
-    
-
