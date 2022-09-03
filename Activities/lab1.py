b779714a5a0670299f46b2829687a9c770aed659
Eliseous-freelance <96338130+Eliseous-freelance@users.noreply.github.com>
2022-09-03 13:40:29 +0100

    Create lab1.py

---
+++ Activities/lab1.py
@@ -0,0 +1,24 @@
+from EduBot_CP import wheelBot_2
+from machine import Pin,ADC
+import board
+from analogio import AnalogIn
+from digitalio import DigitalInOut, Direction, Pull
+
+bot = wheelBot_2(board_type="pico")
+
+digital_pin=Pin(27,Pin.IN)
+analog_pin=ADC(26)
+
+s1 = AnalogIn(board.GP26)
+
+def get_voltage(pin):
+    return (pin.value * 3.3)
+
+s2 = DigitalInOut(board.GP27)
+s2.direction = Direction.INPUT
+
+print(s2.value)
+print(digital_pin.value())
+print(analog_pin.read_u16())
+print(get_intensity(s1))
+
